67204ec8738ac641d277c3fbc12b0eeb25ff7a44
ishkabar <dom.karczewski@gmail.com>
2025-11-02 19:59:43 +0100

    deploy yml11

--- portfolio/src/resources/index.ts
+++ portfolio/src/resources/index.ts
@@ -1,3 +1,2 @@
-export * from './content';
 export * from "./ui-tokens.config";  // baseURL, fonts, style, effects, dataStyle, display, routes, etc.
 export * from "./site.config";       // ogImage, paths
