fdb6ad4a57b656146987e5f0945e483a52f41b5e
ishkabar <dom.karczewski@gmail.com>
2025-11-03 23:07:13 +0100

    Add Google Analytics tracking

--- ogur-next/app/components/analytics.tsx
+++ ogur-next/app/components/analytics.tsx
@@ -1,15 +1,27 @@
 "use client";
+import Script from 'next/script';
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	if (!token) {
-		return null;
-	}
-	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
-	);
+    const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+    if (!gaId) {
+        return null;
+    }
+
+    return (
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaId}');
+                `}
+            </Script>
+        </>
+    );
 }
